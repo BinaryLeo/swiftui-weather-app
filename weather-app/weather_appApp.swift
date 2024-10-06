94e450673691d87168462a5061536221d11e6160
Binaryleo <leomouracss@fastmail.com>
2024-10-06 00:42:39 -0300

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Leonardo Moura on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
